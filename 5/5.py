b514d5c0e34f6086fbce8242689b5508261828da
solokhovir <solokhov.ir@gmail.com>
2024-03-15 16:07:05 +0300

    [UPD] Small fixes

--- 5/5.py
+++ 5/5.py
@@ -3,19 +3,19 @@ X = []
 
 for i in range(0,5):
     x = float(input("Введите x: "))
+    # print("type_x:", type(x))
     X.append(x)
 
 #X=[0.1,0.3, 0.4, 0.7, 1]
 print(' -------------------------')
-print(' | x | s |')
-print(' -----------------------')
+print(' | x         |         s |')
+print(' -------------------------')
 
 for x in X:
     s = 0
     u = -x
     for k in range(1,N+1):
         s += u/k
-        u = -u*x
     print(" | %8.4f | %8.4f |" % (x,s))
 
 print(' -------------------------')
